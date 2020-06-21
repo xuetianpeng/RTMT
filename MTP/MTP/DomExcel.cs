2b2813be4682d8aee8958e56155607f5d59b4463
chooooxtp <chooooxtp@vip.qq.com>
2020-06-21 22:52:46 +0800

    20200621

---
+++ MTP/MTP/DomExcel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace MTP
+{
+    public static class DomExcel
+    {
+        public static void GetSPrintExcel(List<Clients.Client> allclient, int startn, int no)
+        {
+            IWorkbook wb = new XSSFWorkbook();
+            ISheet sh = wb.CreateSheet();
+            sh.PrintSetup.PaperSize = 39;//US Std Fanfold 14 7/8 x 11 in
+            sh.SetMargin(MarginType.BottomMargin, 0);//下方页边距
+            sh.SetMargin(MarginType.TopMargin, 0);//上方页边距
+            sh.SetMargin(MarginType.LeftMargin, 0);//左边页边距
+            sh.SetMargin(MarginType.RightMargin, 0);//右边页边距
+            sh.SetMargin(MarginType.HeaderMargin, 0);//页眉
+            sh.SetMargin(MarginType.FooterMargin, 0);//页脚
+            int pagen = allclient.Count / 32 + 1;//总页数
+            for (int p = 1; p < pagen + 1; p++)//从第1页起，至最后一页
+            {
+                for()
+            }
+        }
+        public static void GetAPrintExcel(List<Clients.Client> allclient)
+        {
+            
+        }
+    }
+}
---
+++ MTP/MTP/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FileInfo[] fis;
+        //选择名条文件夹
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                DirectoryInfo df = new DirectoryInfo(textBox1.Text);
+                fis = df.GetFiles();
+                foreach (FileInfo fi in fis)
+                {
+                    comboBox1.Items.Add(fi.Name);// + " " + fi.FullName);
+                }
+            }
+        }
+
+
+        //选择单独名条文件
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            byte[] ba = File.ReadAllBytes(textBox1.Text + "\\" + comboBox1.Text);
+            label6.Text = Convert.ToInt32(System.Text.Encoding.Default.GetString(ba, ba.Length - 37, 7)).ToString();
+            label7.Text = Convert.ToInt32(ba.Length / 640 / 8 + 1).ToString();
+        }
+
+        //批量打印
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                return;
+            foreach (FileInfo fi in fis)
+            {
+                Print.PrintMT(Clients.Getclient(fi.FullName));
+            }
+        }
+
+
+        //选择打印
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Print.PrintMT(Clients.Getclient(textBox1.Text + @"\" + comboBox1.Text), 
+                Convert.ToInt32(textBox2.Text) - 1, Convert.ToInt32(textBox3.Text) - 1);
+        }
+    }
+}
